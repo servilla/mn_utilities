d7b936523238441fdb5565029b9d03c319fffe8c
Mark Servilla <mark.servilla@gmail.com>
2015-02-09 21:24:16 -0700

    Add docstring comments.

--- src/mn_utilities/node_profile.py
+++ src/mn_utilities/node_profile.py
@@ -4,6 +4,7 @@
 """:Mod: mn_profile
 
 :Synopsis:
+    Simple node profile object with setters and getters for common node attributes.
 
 :Author:
     servilla
@@ -14,44 +15,104 @@
 
 __author__ = "servilla"
 
-class Node_Profile(object):
+class NodeProfile(object):
+    """Simple DataONE node profile object."""
 
     def __init__(self):
+        """Inits NodeProfile with private attributes.
+
+        :return: None
+        """
         self._base_url = None
         self._domain_name = None
         self._cert_path = None
         self._id = None
-        self._mn_version = None
+        self._api_version = None
 
 
-    def set_base_url(self, mn_base_url):
-        self._base_url = mn_base_url
-        path_parts = mn_base_url.split('/')
+    def set_base_url(self, base_url):
+        """Sets the node base URL.
+
+        Sets the node domain name as a side-effect.
+
+        :param base_url: The node base URL
+        :type base_url: str
+        :return: None
+        """
+        self._base_url = base_url
+        path_parts = base_url.split('/')
         self._domain_name = path_parts[2]
 
     def get_base_url(self):
+        """Gets the node base URL.
+
+        :return: The node base URL
+        :rtype: str
+        """
         return self._base_url
 
-    def set_cert_path(self,mn_cert_path):
-        self._cert_path = mn_cert_path
+    def set_cert_path(self,cert_path):
+        """Sets the node x509 certificate path.
+
+        :param cert_path: The x509 certificate path
+        :type cert_path: str
+        :return: None
+        """
+        self._cert_path = cert_path
 
     def get_cert_path(self):
+        """Gets the node x509 certificate path.
+
+        :return: The x509 certificate path
+        :rtype: str
+        """
         return self._cert_path
 
-    def set_id(self, mn_id):
-        self._id = mn_id
+    def set_id(self, id):
+        """Sets the canonical node identifier.
+
+        :param id: The node identifier
+        :type id: str
+        :return: None
+        """
+        self._id = id
 
     def get_id(self, esc=False):
-        mn_id = self._id
+        """Gets the canonical node identifier.
+
+        :param esc: Flag to escape the node identifier colons
+        :type esc: bool
+        :return: The node identifier
+        :rtype: str
+        """
+        id = self._id
         if esc:
-            mn_id = mn_id.replace(":", "\:")
-        return mn_id
+            id = id.replace(":", "\:")
+        return id
 
     def get_domain_name(self):
+        """Gets the node's Internet domain name.
+
+        The Internet domain name is set as a side-effect of the set_base_url function.
+
+        :return: The node's domain name
+        :rtype: str
+        """
         return self._domain_name
 
-    def set_version(self, version):
-        self._mn_version = version
+    def set_api_version(self, api_version):
+        """Sets the node's DataONE API version.
 
-    def get_version(self):
-        return self._mn_version
+        :param api_version: The node's API version
+        :type api_version: str
+        :return: None
+        """
+        self._api_version = api_version
+
+    def get_api_version(self):
+        """Gets the node's DataONE API version.
+
+        :return: The node's API version
+        :rtype: str
+        """
+        return self._api_version
